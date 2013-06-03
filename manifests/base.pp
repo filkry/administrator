e19a84d8237da663f6543023cf9f1334b49bfd09
Fil Krynicki <filipkrynicki@gmail.com>
2013-06-03 16:37:19 -0400

    too frustrated to continue right now

---
+++ manifests/base.pp
@@ -0,0 +1,52 @@
+exec { "apt-get update":
+    command => "/usr/bin/apt-get update",
+}
+
+package {"nginx":
+    ensure => present,
+	require => Exec['apt-get update'],
+}
+
+package {"gunicorn":
+	ensure => present,
+	require => Exec['apt-get update'],
+}
+
+package {"python":
+	ensure => present,
+	require => Exec['apt-get update'],
+}
+
+package {"python-pip":
+	ensure => present,
+	require => [Exec['apt-get update'],
+			    Package['python']],
+}
+
+package { "flask": 
+    require => Package["python-pip"],
+    ensure  => latest,
+    provider => pip,
+}
+
+service { 'nginx_service':
+	ensure => running,
+	enable => true,
+	require => Package['nginx'],
+	# I don't know what these do
+	# hasstatus => true,
+	# hasrestart => true,
+}
+
+file {"nginx-default":
+    path => '/etc/nginx/sites-enabled/default',
+    ensure => absent,
+    require => Package['nginx'],
+}
+
+file {"administrator_nginx":
+    path => '/etc/nginx/sites-enabled/administrator_nginx',
+    ensure => present,
+    require => Package['nginx'],
+    source => "/vagrant/config/administrator_nginx",
+}
